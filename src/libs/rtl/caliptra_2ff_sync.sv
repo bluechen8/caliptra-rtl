e53132fe13f5027a169620a77123f5af82dcaae9
Jim Fang <yf328@eecs.berkeley.edu>
2026-03-16 13:35:39 -0700

    add: first round of cadence specific cells modifications

--- src/libs/rtl/caliptra_2ff_sync.sv
+++ src/libs/rtl/caliptra_2ff_sync.sv
@@ -22,18 +22,46 @@ module caliptra_2ff_sync  #( parameter WIDTH=1,
 
 );
 
-logic din_ff;
+// logic din_ff;
 
-always_ff@(posedge clk or negedge rst_b) begin
-    if(!rst_b) begin
-        dout <= RST_VAL;
-        din_ff <= RST_VAL;
-    end
-    else begin 
-        din_ff <= din;
-        dout <= din_ff;
-    end
-end
+// always_ff@(posedge clk or negedge rst_b) begin
+//     if(!rst_b) begin
+//         dout <= RST_VAL;
+//         din_ff <= RST_VAL;
+//     end
+//     else begin 
+//         din_ff <= din;
+//         dout <= din_ff;
+//     end
+// end
+
+genvar i;
+   logic [WIDTH-1:0] stage1;
+
+   generate
+      // Loop through each bit of the bus
+      for (i = 0; i < WIDTH; i++) begin : gen_sync_cell
+         
+         // Instantiate the specific Foundry Synchronizer Cell
+         // REPLACE 'FOUNDRY_SYNC_CELL_NAME' with the actual cell name (e.g., SYNC2_D1_SS)
+
+         // need to chain 2 flip flops together to create a sync cell
+      DFFX1 u_sync_ff0 (
+        .CK (clk),
+        .D  (din[i]),
+        .Q  (stage1[i]),
+        .QN ()          // optional, leave unconnected
+      );
+
+      DFFX1 u_sync_ff1 (
+        .CK (clk),
+        .D  (stage1[i]),
+        .Q  (dout[i]),
+        .QN ()
+      );
+      end
+endgenerate
+
 
 
 endmodule
